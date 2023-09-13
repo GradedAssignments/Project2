033712ced877d50c3ea30dc16b49fc72df51aeba
Aman <142117059+GradedAssignments@users.noreply.github.com>
2023-09-13 18:21:31 +0530

    new 1
    1

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDspWPtK1XoYbgHntJhGIp6qpWymkWflPk",
+  authDomain: "temporary-ebeef.firebaseapp.com",
+  projectId: "temporary-ebeef",
+  storageBucket: "temporary-ebeef.appspot.com",
+  messagingSenderId: "821363183100",
+  appId: "1:821363183100:web:02b0fd1d5188c8af76b679",
+  measurementId: "G-B9NPEJR9XR"
+};
+
+export default firebaseConfig;
